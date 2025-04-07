97649a3ecf6f801b2c62f0e834363d35c966a66e
Lil-Leoz <a.fvenom2.0@gmail.com>
2025-04-07 16:57:36 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -82,7 +82,7 @@ router.get("/", async (req, res) => {
             );
 
             const sid = `Sonic-MD BOT PAIR\n\nWeb pair msg`;
-            const mg = `🛑 *Do not share this code to anyone* 🛑\n\n> ROBIN MAX`
+            const mg = `🛑 *Do not share this code to anyone* 🛑\n\n> Sonic-MD`
             await RobinPairWeb.sendMessage(user_jid, {
               image: {
                 url: "https://raw.githubusercontent.com/Lil-Leoz/bot-help/refs/heads/main/IMG-20250407-WA0004.jpg",
